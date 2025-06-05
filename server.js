76341061ee1223f05df7fecad3f7d849623bf2ff
Viktoriia Kovalenko <rambambam72@gmail.com>
2025-06-05 14:47:58 +0300

    add root get endpoint

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ app.use(cors({
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerApiDoc))
 app.use('/', indexRouter)
 
+app.get('/', (req, res) => {
+    res.send('Hello from Pizza Store on Render!')
+})
+
 app.use(errorHandler)
 
 server.listen(port, () => {
